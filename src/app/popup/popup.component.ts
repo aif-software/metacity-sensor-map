6bf088361595442bc4fe703ee5f7c655605e6ea2
otto <otto2kalliokoski@gmail.com>
2025-05-26 15:42:33 +0300

    Add @Input for weatherData

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
 })
 export class PopupComponent {
   @Input() id?: string;
+  @Input() name?: string;
   @Input() description?: string;
   @Input() location?: string;
   @Input() elevation?: number;
@@ -21,4 +22,18 @@ export class PopupComponent {
   @Input() stationary?: string;
   @Input() dataValue?: string;
   @Input() linkToData?: string;
+
+  @Input() air_temperature?: number;
+  @Input() air_relative_humidity?: number;
+  @Input() dew_point_temperature?: number;
+  @Input() wind_speed?: number;
+  @Input() wind_direction?: number;
+  @Input() rainfall_depth?: number;
+  @Input() rainfall_intensity?: number;
+  @Input() snow_depth_a?: number;
+  @Input() snow_depth_b?: number;
+  @Input() snow_depth_c?: number;
+  @Input() snow_depth?: number;
+  @Input() rain_classification?: number;
+  @Input() road_surface_temperature?: number;
 }
